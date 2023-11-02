b76db6a9141268060f1fde90c8412db2fcd058e4
Rowan <rowan@bigsmoke.us>
2023-11-02 09:46:05 +0000

    Try to fix for GNU bash, version 5.2.2(1)-release (x86_64-apple-darwin21.6.0)

--- bin/with_cmdqd.sh
+++ bin/with_cmdqd.sh
@@ -3,20 +3,29 @@
 : "${CMDQD_SIGUSR1_TIMEOUT:=2s}"
 SCRIPT_NAME=$(basename "$0")
 
-# TODO: parse -d from $@
+# `psql` will only return exit codes between 0 and 3; therefore any exit code > 3 will be distinguishable.
+EXIT_CODE_ON_CMDQD_SIGUSR1_TIMEOUT=4
+EXIT_CODE_ON_CMDQD_FAILURE=5
+
+# Remember the argument array as a global variable, so that we can later access it from a function's scope.
 ARGV=("$@")
+# TODO: parse -d from $@
 
 clean_exit() {
     local exit_code="${1:-0}"
-    if [[ "$(ps -p $cmdqd_pid -o comm=)" != "$(basename "$CMDQD_BIN")" ]]; then
-        wait $cmdqd_pid
-        exit_code=$?
-        if [[ $exit_code != 0 ]]; then
-            echo -e "\x1b[31m\x1b[1m$(basename "$CMDQD_BIN")\x1b[22m failed with exit code \x1b[1m$exit_code\x1b[22m.\x1b[0m"
-            exit 3
+
+    # Too bad that we can't just peak in `/proc/$cmdqd_pid` on MacOS.
+    if [[ "$(basename "$(ps -p $cmdqd_pid -o comm=)" )" != "$(basename "$CMDQD_BIN")" ]]; then
+        wait $cmdqd_pid  # The `pg_cmdqd` process is no longer running; so `wait` should return immediately.
+        cmdqd_exit_code=$?
+        if [[ $cmdqd_exit_code != 0 ]]; then
+            echo -e "\x1b[31m\x1b[1m$(basename "$CMDQD_BIN")\x1b[22m failed with exit code \x1b[1m$cmdqd_exit_code\x1b[22m.\x1b[0m"
+            exit_code=$EXIT_CODE_ON_CMDQD_FAILURE
         fi
-    else
-        kill "$cmdqd_pid"
+    else  # `pg_cmdqd` is still running.
+        kill "$cmdqd_pid"  # Send `SIGTERM` to `pg_cmdqd`.
+        wait "$cmdqd_pid"
+        cmdqd_exit_code=$?
 
         # `kill -0` just checks if the process is available to send a signal _to_; i.e., is it still running?
         while kill -0 "$cmdqd_pid" >/dev/null 2>&1; do
@@ -29,12 +38,23 @@ clean_exit() {
     fi
 
     exit $exit_code
+    # From https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#exit :
+    # > A trap on EXIT shall be executed before the shell terminates, except when the exit utility is invoked
+    # > in that trap itself, in which case the shell shall exit immediately.
 }
 
+# `SIGUSR1` trap handler that launches the `psql` command constructed by our `pg_regress` parent process.
 receive_sigusr1() {
     kill "$timer_pid"; timer_pid=
-    trap - SIGUSR2
+
+    # Ignore `SIGUSR2` rather than restore the default (termination) action in case the timer isn't dead yet
+    # so soon after the kill.
+    trap '' SIGUSR2
+
+    # Run the `psql` command that `pg_regress` has us be the launcher to.
     "${ARGV[@]}"
+
+    clean_exit 0
 }
 
 # The only way I could think of to make this script time out was to background a timer child process,
@@ -44,15 +64,17 @@ receive_sigusr1() {
 #   (b) let the child emit a `SIGUSR2` signal back to the parent, which will than prompt us to
 #       initiate a clean exit (_with_ error code, mind you).
 send_sigusr2_after_timeout() {
-    local pid=$1
+    local main_pid=$1
     sleep "$CMDQD_SIGUSR1_TIMEOUT"
-    kill -SIGUSR2 $pid
+    kill -SIGUSR2 $main_pid
 }
 
+# `SIGUSR2` trap handler that forcefully kills `pg_cmdqd` and then commits seppuku when the daemon fails to
+# report its readiness within the `$CMDQD_SIGUSR1_TIMEOUT`.
 receive_sigusr2() {
     echo -e "\x1b[31m$CMDQD_SIGUSR1_TIMEOUT timeout expired while waiting to receive the \x1b[1mSIGUSR1\x1b[22m signal from \x1b[1m$(basename "$CMDQD_BIN")\x1b[22m.\x1b[0m"
     timer_pid=
-    clean_exit 4
+    clean_exit $EXIT_CODE_ON_CMDQD_SIGUSR1_TIMEOUT
 }
 
 export PGDATABASE="contrib_regression"
